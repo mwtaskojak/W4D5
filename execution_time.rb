2d5b06d350225e672b45e464d71f0e70fa1b866c
mwtas <mwtasemalkojak@gmail.com>
2021-03-20 02:24:50 -0400

    find largest subsum

--- execution_time.rb
+++ execution_time.rb
@@ -1,4 +1,4 @@
-list = [ 0, 3, 5, 4, -5, 10, 1, 90 ]
+# list = [ 0, 3, 5, 4, -5, 10, 1, 90 ]
     def sm_min(arr)
         min = arr.first
         arr.each do |ele|    # O(N)
@@ -35,8 +35,43 @@ list = [ 0, 3, 5, 4, -5, 10, 1, 90 ]
        arr.each { |num| min_num = num if num < min_num }
       
         min_num
-      end
-    end 
+    end
+    
+
+
 
     
+    # Phase I
+    # Write a function that iterates through the array and finds all sub-arrays using nested loops. 
+    # First make an array to hold all sub-arrays. Then find the sums of each sub-array and return the max.
+
+    #ist = [5, 3, -7]
+    # largest_contiguous_subsum(list) # => 8
+
+    # possible sub-sums
+   p [5]           # => 5
+    [5, 3]        # => 8 --> we want this one
+    [5, 3, -7]    # => 1
+   p [3]           # => 3
+    [3, -7]       # => -4
+   p [-7]          # => -7
+    
+        require 'byebug'
       
+        def largest_contiguous_subsum(arr)
+            debugger
+            subs = []
+
+            arr.each_index do |idx1|
+                 (idx1..array.length - 1).each do |idx2|
+                     subs << array[idx1..idx2]
+                 end
+             end
+
+                subs.map { |sub| sub.inject(:+) }.max
+            
+        end 
+
+      
+
+       
